d9fe2396a9a46477c52846579af7db9b542967ca
Manuel Stühn <manuel.stuehn@gmx.net>
2024-04-02 14:40:47 +0200

    Complete interrupt implementation

--- src/main.rs
+++ src/main.rs
@@ -19,20 +19,31 @@ use cortex_m_rt::entry;
 use cortex_m::interrupt::{free, Mutex};
 use usb_device::device::StringDescriptors;
 use usb_device::prelude::*;
-use usbd_serial::{SerialPort, USB_CLASS_CDC};
+use usb_device::class_prelude::UsbBusAllocator;
+use usbd_serial::{
+    SerialPort,
+    USB_CLASS_CDC
+};
 
 use core::{
     cell::RefCell,
     ops::DerefMut
 };
 
-static mut EP_MEMORY: [u32; 1024] = [0; 1024];
-
 static BUTTON: Mutex<RefCell<Option<PA0<Input>>>> = Mutex::new(RefCell::new(None));
 static LED_HANDLER: Mutex<RefCell<Option<PD12<Output>>>> = Mutex::new(RefCell::new(None));
 
+static G_USB_SERIAL: Mutex<RefCell<Option<SerialPort<UsbBus<USB>>>>> =
+    Mutex::new(RefCell::new(None));
+
+static G_USB_DEVICE: Mutex<RefCell<Option<UsbDevice<UsbBus<USB>>>>> =
+    Mutex::new(RefCell::new(None));
+
 #[entry]
 fn main() -> ! {
+    static mut USB_BUS: Option<UsbBusAllocator<stm32f4xx_hal::otg_fs::UsbBusType>> = None;
+    static mut EP_MEMORY: [u32; 1024] = [0; 1024];
+
 
     rtt_init_print!();
     rprintln!("Starting Controller");
@@ -51,7 +62,6 @@ fn main() -> ! {
         .freeze();
 
     let gpioa = dp.GPIOA.split();
-    //let button = gpioa.pa0.into_input();
 
     let mut board_btn = gpioa.pa0.into_pull_down_input();
     board_btn.make_interrupt_source(&mut syscfg);
@@ -71,12 +81,14 @@ fn main() -> ! {
     };
     rprintln!("USB set up");
 
-    let usb_bus = UsbBus::new(usb, unsafe { &mut EP_MEMORY });
+    USB_BUS.replace(stm32f4xx_hal::otg_fs::UsbBusType::new(usb, EP_MEMORY));
 
-    let mut serial = SerialPort::new(&usb_bus);
+    let local_usb_bus = USB_BUS.as_ref().unwrap();
+
+    let serial = SerialPort::new(&local_usb_bus);
     rprintln!("Serial set up");
 
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+    let usb_dev = UsbDeviceBuilder::new(&local_usb_bus, UsbVidPid(0x16c0, 0x27dd))
         .strings(&[StringDescriptors::default()
             .manufacturer("Fake company")
             .product("Serial port")
@@ -88,19 +100,63 @@ fn main() -> ! {
     free(|cs| {
         BUTTON.borrow(cs).replace(Some(board_btn));
         LED_HANDLER.borrow(cs).replace(Some(green));
+
+        G_USB_SERIAL.borrow(cs).replace(Some(serial));
+        G_USB_DEVICE.borrow(cs).replace(Some(usb_dev));
     });
+    rprintln!("Handlers registered ");
 
     pac::NVIC::unpend(hal::pac::Interrupt::EXTI0);
+    pac::NVIC::unpend(hal::pac::Interrupt::OTG_FS);
     unsafe {
+        pac::NVIC::unmask(hal::pac::Interrupt::OTG_FS);
         pac::NVIC::unmask(hal::pac::Interrupt::EXTI0);
     };
+    rprintln!("Start loop");
 
+    #[allow(clippy::empty_loop)]
     loop {
 
-        if !usb_dev.poll(&mut [&mut serial]) {
-            continue;
-        }
+    }
+}
 
+#[allow(non_snake_case)]
+#[interrupt]
+fn EXTI0() {
+    free(|cs| {
+        rprintln!("EXTI0 Interrupt");
+        let mut btn_ref = BUTTON.borrow(cs).borrow_mut();
+        if let Some(ref mut btn) = btn_ref.deref_mut() {
+            btn.clear_interrupt_pending_bit();
+        }
+        let mut grn_ref = LED_HANDLER.borrow(cs).borrow_mut();
+        if let Some(ref mut grn) = grn_ref.deref_mut() {
+            grn.toggle();
+        }
+    });
+}
+
+#[allow(non_snake_case)]
+#[interrupt]
+fn OTG_FS() {
+    static mut USB_SERIAL: Option<SerialPort<UsbBus<USB>>> = None;
+    static mut USB_DEVICE: Option<UsbDevice<UsbBus<USB>>> = None;
+
+    let usb_dev = USB_DEVICE.get_or_insert_with(|| {
+        cortex_m::interrupt::free(|cs| {
+            // Move USB device here, leaving a None in its place
+            G_USB_DEVICE.borrow(cs).replace(None).unwrap()
+        })
+    });
+
+    let serial = USB_SERIAL.get_or_insert_with(|| {
+        cortex_m::interrupt::free(|cs| {
+            // Move USB serial device here, leaving a None in its place
+            G_USB_SERIAL.borrow(cs).replace(None).unwrap()
+        })
+    });
+
+    if usb_dev.poll(&mut [serial]) {
         let mut buf = [0u8; 64];
 
         match serial.read(&mut buf) {
@@ -142,20 +198,5 @@ fn main() -> ! {
             }
         }
     }
-}
-
-#[allow(non_snake_case)]
-#[interrupt]
-fn EXTI0() {
-    free(|cs| {
-        let mut btn_ref = BUTTON.borrow(cs).borrow_mut();
-        if let Some(ref mut btn) = btn_ref.deref_mut() {
-            btn.clear_interrupt_pending_bit();
-        }
-        let mut grn_ref = LED_HANDLER.borrow(cs).borrow_mut();
-        if let Some(ref mut grn) = grn_ref.deref_mut() {
-            grn.toggle();
-        }
-    });
 }
 
